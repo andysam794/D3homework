9ddb7f65e9b93f3dd952fb6fde40aea3220c38bf
andysam794 <107290713+andysam794@users.noreply.github.com>
2022-06-30 00:31:38 +0800

    Update main-scatter.js

--- assets/js/main-scatter.js
+++ assets/js/main-scatter.js
@@ -230,7 +230,7 @@ function ready(movies) {
 }
 
 //Load Data
-d3.csv('D3homework/D3.js/scatter_chart_reactive(1)/movies.csv', type).then(
+d3.csv('https://andysam794.github.io/D3homework/D3.js/scatter_chart_reactive(1)/movies.csv', type).then(
     res => {
         ready(res);
         // console.log(res);
